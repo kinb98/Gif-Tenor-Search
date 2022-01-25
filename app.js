8eb4138f543f240b12aab85344c308db5da4aa18
kinb98 <kinjalkbajpai@gmail.com>
2022-01-25 19:13:09 +0530

    Adding Middleware

--- app.js
+++ app.js
@@ -5,13 +5,21 @@ const express = require("express");
 const app = express();
 
 //Middleware
+const { engine } = require("express-handlebars");
+app.engine("handlebars", engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 //Routes
 app.get("/", (req, res) => {
-  res.send("Hello Squirrel");
+  const gifUrl =
+    "https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245";
+  res.render("hello-gif", { gifUrl });
 });
 
-app.get("/:username", (req, res) => {});
+app.get("/greetings/:name", (req, res) => {
+  const name = req.params.name;
+  res.render("greetings", { name });
+});
 
 //Start Server
 app.listen(3000, () => {
